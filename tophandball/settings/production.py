05518640489aac196e20fab50f035673650cc514
Leticia Hernandez <leticia.hu@gmail.com>
2015-05-16 19:03:36 +0200

    Changes after deploy

--- tophandball/settings/production.py
+++ tophandball/settings/production.py
@@ -18,13 +18,14 @@ SERVER_EMAIL = 'support@tophandball.com'
 
 ALLOWED_HOSTS = [
     '.tophandball.com',  # Allow domain and subdomains
+    'localhost',
 ]
 
 # Security
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_HTTPONLY = True
-X_FRAME_OPTIONS = 'DENY'
+# CSRF_COOKIE_SECURE = True
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_HTTPONLY = True
+# X_FRAME_OPTIONS = 'DENY'
 
 # Performance settings, need testing.
 CONN_MAX_AGE = 60
